116186d2716ba87ef304a8083497bf4bce33a031
fuomag9 <fuomag@gmail.com>
2021-06-20 12:20:48 +0200

    added iterations over directories and subdirectories

--- Bash/rec_search.sh
+++ Bash/rec_search.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-for file in $1/*  ; do 
-    if [[ -f "$file" ]] ; then
-        # Faccio cose  
-    elif [[ -d "$file" ]] ; then
-        echo "Ricorsione nella directory $file"
-        # Chiamata ricorsiva
-        "$0" "$file" "$2" "$3"
-    fi
+for file in $1/*; do
+  if [[ -f "$file" ]]; then
+    # Faccio cose
+  elif [[ -d "$file" ]]; then
+    echo "Ricorsione nella directories $file"
+    # Chiamata ricorsiva
+    "$0" "$file" "$2" "$3"
+  fi
 done
 ##############################################
 #############      NOTA BENE     #############
@@ -17,6 +17,49 @@ done
 #USARE PWD SOLO SE RICHIESTO, OPPURE SE SI SCRIVE CD $1 PER RIFERIRSI ALLA CARTELLA ATTUALE
 
 ## Controllare che un file (senza percorso assoluto, solo nome del file) inizi per una determinata stringa
-if [[ `basename $file` == "$2"* ]] ; then
-    # Inserire codice qui
+if [[ $(basename $file) == "$2"* ]]; then
+  # Inserire codice qui
 fi
+
+# Iterate over all subdirectories
+directories=()
+while IFS= read -r -d $'\0'; do
+  directories+=("$REPLY")
+done < <(find "$1" -type d -print0)
+for dir in "${directories[@]}"; do
+  echo "$dir"
+done
+
+# Iterate over all files in a directory BUT ONLY THE CURRENT ONE
+files=()
+while IFS= read -r -d $'\0'; do
+  files+=("$REPLY")
+done < <(find "$1" -maxdepth 1 -type f -print0)
+for file in "${files[@]}"; do
+  echo "$file"
+done
+
+# Iterate over all files in a directory AND SUBDIRECTORIES
+files=()
+while IFS= read -r -d $'\0'; do
+  files+=("$REPLY")
+done < <(find "$1" -type f -print0)
+for file in "${files[@]}"; do
+  echo "$file"
+done
+
+#Iterate over all subdirectories and ALL THE FILES in those subdirectoies
+
+directories=()
+while IFS= read -r -d $'\0'; do
+  directories+=("$REPLY")
+done < <(find "$1" -type d -print0)
+for dir in "${directories[@]}"; do
+  files=()
+  while IFS= read -r -d $'\0'; do
+    files+=("$REPLY")
+  done < <(find "$dir" -type f -print0)
+  for file in "${files[@]}"; do
+    echo "$file"
+  done
+done
